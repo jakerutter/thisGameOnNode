3fe26f0116f78f1c0e1b7f9100863fb2dc35aa92
jakerutter <rutter.jacob@gmail.com>
2019-06-04 17:34:07 -0400

    changed tests, preparing to test from beginning to last working functionality then continue developing

--- cypress/integration/pregame.spec.js
+++ cypress/integration/pregame.spec.js
@@ -1,19 +1,33 @@
-{/* <reference types="Cypress" /> */}
-
+//navigate and log into the chat
 context('Navigation', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:8080/')
     cy.get('#playerName').click()
     cy.get('#playerName').type('player1')
     cy.get('#confirmSettings').click()
   })
 
-
-describe('My first passing test', function() {
-    it('Does not do much!', function() {
-        expect(true).to.equal(true);
-    })
+describe('Player1 button displayed in banner at top', function() {
+  it('Appears at top of screen', function() {
+    cy.get('#currentUsers').contains('player1');
+  })
 })
+
+describe('Player1 can click into the text bar and type a message', function(){
+  it('Text appears in input bar of chat room', function(){
+    cy.get('#m').click()
+    cy.get('#m').type('Testing the chat input')
+    cy.get('#m').contains('Testing the chat input')
+  })
+})
+
+describe('Player1 can submit chat text into the chat forum', function(){
+  it('Text appears in the chat room', function(){
+    cy.get('#m').click().type('{enter}')
+    cy.get('#chatFeature').contains('Testing the chat input')
+  })
+})
+
 
 
 })
