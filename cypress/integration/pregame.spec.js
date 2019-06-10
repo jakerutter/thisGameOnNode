a1a2a37af01308f3c1064e801c3a655c41d0f398
jakerutter <rutter.jacob@gmail.com>
2019-06-09 21:31:37 -0400

    added additional cypress tests for choosing settings

--- cypress/integration/pregame.spec.js
+++ cypress/integration/pregame.spec.js
@@ -35,5 +35,48 @@ describe('Player1 can click on Jake\'s player button', function(){
   })
 })
 
+describe('Welcome modal is displayed when the challenge is accepted', function(){
+  it('Welcome modal appears for player1', function(){
+    cy.get('#welcomeModal').invoke('show')
+    cy.get('#welcomeModal').contains('Select a color')
+  })
+})
+
+describe('Clicks on the settings button and can read the game pemise', function(){
+  it('Opens the info modal', function(){
+    cy.get('#btnSettingsModal').click()
+    cy.get('#welcomeModal').invoke('hide')
+    cy.get('#settingsModal').invoke('show')
+    cy.get('#settingsModal').contains('A Rutter Production. 2018.')
+  })
+})
+
+describe('Clicks the continue button and is returned to the previous screen', function(){
+  it('Returns to the modal for color selection', function(){
+    cy.get('#btnHideSettingsModal').click()
+    cy.get('#settingsModal').invoke('hide')
+    cy.get('#welcomeModal').invoke('show')
+    cy.get('#welcomeModal').contains('Select a color')
+  })
+})
+
+describe('Color dropdown has all available color options', function(){
+  it('Has red, orange, yellow, green, blue, indigo, violet colors', function(){
+    cy.get('#playerColorSelect')
+      .select('red').should('have.value', 'red')
+      .select('orange').should('have.value', 'orange')
+      .select('yellow').should('have.value', 'yellow')
+      .select('green').should('have.value', 'green')
+      .select('blue').should('have.value', 'blue')
+      .select('indigo').should('have.value', 'indigo')
+      .select('violet').should('have.value', 'violet')
+  })
+})
+describe('Clicks the color selector and chooses red', function(){
+  it('Selects red as Player1\'s color', function(){
+    cy.get('#playerColorSelect').select('red')
+    cy.get('#confirmSettings').click()
+  })
+})
 
 })
